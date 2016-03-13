83bc2ef5a311d8936a32b1725febb92b22a5a49f
synergistics <kingjak678@gmail.com>
2016-03-13 11:52:15 +0000

    Adds zsh/.zsh folder that was missing

---
+++ zsh/.zsh/exports.zsh
@@ -0,0 +1,10 @@
+# Ensures charset is correct
+export LANG=en_US.UTF-8
+
+export TERM=rxvt-unicode-256color
+
+export MANPAGER='most'
+export EDITOR='vim'
+
+export NVM_DIR="/home/jking/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
---
+++ zsh/.zsh/prompt.zsh
@@ -0,0 +1,2 @@
+PROMPT="%{$fg[cyan]%}%n@%m in %/%{$reset_color%}
+%{$fg[cyan]%}――― ―― %{$reset_color%}"
