360bc07259f2b0aaa5f5edc7f31ec5be88e9a360
synergistics <kingjak678@gmail.com>
2016-03-13 23:18:03 +0000

    Removes alias ls for fancy ls

--- zsh/.zsh/aliases.zsh
+++ zsh/.zsh/aliases.zsh
@@ -1,6 +1,3 @@
-# Colorize output, add filetype indicator, human readable file sizes
-alias ls='ls --color -GFh'
-
 # Same as above, but list all files and in long format
 alias ll='ls --color -GFhla'
 
